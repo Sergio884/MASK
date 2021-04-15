3841bfc4b0f386db2d777d0245c7b1d3a30fd983
Sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2021-04-14 20:48:50 -0500

    Despliegue de mensajes del chat

--- php/chat.php
+++ php/chat.php
@@ -4,9 +4,46 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="../Estilos/estiloLogin.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../estilos/tablaEstilos.css">
     <title>Chat</title>
   </head>
   <body>
+  <?php
+    include('dbconnection.php');
+    $usuario = $_GET['usuario'];
+    $query = "CREATE TABLE IF NOT EXISTS chats".$usuario."(idChat INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    chat VARCHAR(106),
+    ultimoMensaje VARCHAR(999),
+    tiempo TIMESTAMP,
+    usuario VARCHAR(50),
+    estado VARCHAR(20))";
+    $run = mysqli_query($connection,$query);
+    mysqli_close($connection);
+  ?>
+    <table class="contenidoTabla">
+    <thead>
+      <tr>
+        <th>Emisor</th>
+        <th>Mensaje</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      include('dbconnection.php');
+      $ordenar = "SELECT * FROM chatsergio884_clairo
+                  ORDER BY idMensaje DESC";
+
+      $run = mysqli_query($connection,$ordenar);
+      while($resultado = mysqli_fetch_assoc($run)){
+         ?>
+      <tr>
+        <td><?php echo $resultado['emisor'].":"; ?></td>
+        <td><?php echo $resultado['mensaje']; ?></td>
+      </tr>
+    <?php } ?>
+
+    </tbody>
+  </table>
 
   </body>
 </html>
