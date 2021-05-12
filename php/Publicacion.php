21aad7af4ab754c8dc6ad722023d5d10ec3fcbe7
AxelGarciaTello <gata21022000@hotmail.com>
2021-05-12 12:53:55 -0500

    Limpieza del código

--- php/Publicacion.php
+++ php/Publicacion.php
@@ -1,5 +1,81 @@
-<!doctype html>
-<html lang="en">
+<?php
+  $idInmueble = $_GET['IdInmueble'];
+  include('dbconnection.php');
+
+  /*Información del inmueble*/
+  $sql = "SELECT * FROM Inmueble WHERE IdInmueble=".$idInmueble."";
+  $result = mysqli_query($connection, $sql);
+  if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_assoc($result);
+    $NumeroDormitorio = $row['NumeroDormitorios'];
+    $NumeroBanios = $row['NumeroBanios'];
+    $VentaRenta = $row['VentaRenta'];
+    $TipoInmueble = $row['TipoInmueble'];
+    $Titulo = $row['Titulo'];
+    $Estado = $row['Estado'];
+    $Ciudad = $row['Ciudad'];
+    $Direccion = $row['Direccion'];
+    $CP = $row['CP'];
+    $MetrosCuadrados = $row['MetrosCuadrados'];
+    $Descripcion = $row['Descripcion'];
+    $Costo = $row['Costo'];
+    $Moneda = $row['Moneda'];
+    $Longitud = $row['Longitud'];
+    $Latitud = $row['Latitud'];
+    $IdUsuario = $row['IdUsuario'];
+    $Visitas = $row['Visitas'];
+    $Visitas = $Visitas + 1;
+  }
+
+  /*Información del vendedor*/
+  $sql = "UPDATE inmueble SET Visitas = ".$Visitas." WHERE idInmueble = ".$idInmueble."";
+  if(mysqli_query($connection, $sql)){
+  }
+  $sql = "SELECT * FROM Usuario WHERE IdUsuario = ".$IdUsuario."";
+  $result = mysqli_query($connection, $sql);
+  if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_assoc($result);
+    $Usuario = $row['Usuario'];
+    $Correo = $row['Correo'];
+    $Calificacion = $row['Calificacion'];
+    $Telefono = $row['Telefono'];
+    $ImagenUsuario = $row['FotoUsuario'];
+    if($Calificacion < 0.5){
+      $Calificacion = "<img src=\"../imagenes/estrella00.png\">";
+    }
+    elseif ($Calificacion < 1) {
+      $Calificacion = "<img src=\"../imagenes/estrella05.png\">";
+    }
+    elseif ($Calificacion < 1.5) {
+      $Calificacion = "<img src=\"../imagenes/estrella10.png\">";
+    }
+    elseif ($Calificacion < 2) {
+      $Calificacion = "<img src=\"../imagenes/estrella15.png\">";
+    }
+    elseif ($Calificacion < 2.5) {
+      $Calificacion = "<img src=\"../imagenes/estrella20.png\">";
+    }
+    elseif ($Calificacion < 3) {
+      $Calificacion = "<img src=\"../imagenes/estrella25.png\">";
+    }
+    elseif ($Calificacion < 3.5) {
+      $Calificacion = "<img src=\"../imagenes/estrella30.png\">";
+    }
+    elseif ($Calificacion < 4) {
+      $Calificacion = "<img src=\"../imagenes/estrella35.png\">";
+    }
+    elseif ($Calificacion < 4.5) {
+      $Calificacion = "<img src=\"../imagenes/estrella40.png\">";
+    }
+    elseif ($Calificacion < 4.8) {
+      $Calificacion = "<img src=\"../imagenes/estrella45.png\">";
+    }
+    else {
+      $Calificacion = "<img src=\"../imagenes/estrella50.png\">";
+    }
+  }
+ ?>
+<html>
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -38,12 +114,29 @@
     <header>
       <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-          <span id="orden">
-          </span>
+          <?php
+            /*Información de las imagenes*/
+            $sql = "SELECT * FROM InmuebleFoto WHERE IdInmueble=".$idInmueble."";
+            $result = mysqli_query($connection, $sql);
+            if(mysqli_num_rows($result) > 0){
+              echo "<button type=\"button\" data-bs-target=\"#myCarousel\" data-bs-slide-to=\"0\" class=\"active\" aria-current=\"true\" aria-label=\"Slide 1\"></button>";
+              $Contador = 1;
+              while($Contador < mysqli_num_rows($result)){
+                echo "<button type=\"button\" data-bs-target=\"#myCarousel\" data-bs-slide-to=\"".$Contador."\" aria-label=\"Slide ".($Contador+1)."\"></buton>";
+                $Contador = $Contador + 1;
+              }
+            }
+           ?>
         </div>
         <div class="carousel-inner">
-          <span id="imagenes">
-        </span>
+          <?php
+            /*Información de las imagenes*/
+            $Activado = "active";
+            while ($row = mysqli_fetch_array($result)) {
+              echo "<div class=\"carousel-item ".$Activado."\"><img class=\"d-block w-100\" src=\"data:image/jpg;base64, ".base64_encode($row['Foto'])."\"></div>";
+              $Activado = "";
+            }
+           ?>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -54,31 +147,29 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-
       <div class="row g-3">
-
         <div class="col-md-7 col-lg-8">
-          <h1 id="Titulo">Aqui va el Titulo</h1>
-          <h2><span  id="Costo">Aqui va el precio</span> <span id="Moneda">MX</span></h2>
-          <h3> <span id="Estado">México</span> <span id="Ciudad">México</span> </h3>
-          <h3><span id="Direccion">Juan Jose</span> <span id="CP">56883</span></h3>
-          <p id="VentaRenta">Venta</p>
-          <p id="TipoInmueble">Departamento</p>
-          <p>Metros cuadrados: <span id="MetrosCuadrados">100</span>m²</p>
-          <p>N&uacute;mero de dormitorios: <span id="NumeroDormitorio">2</span></p>
-          <p>N&uacute;mero de baños: <span id="NumeroBanios">1</span></p>
-          <p>Longitud: <span id="Longitud">100</span> Latitud<span id="Latitud">100</span></p>
+          <h1><?php echo $Titulo; ?></h1>
+          <h2>$<?php echo $Costo; ?></h2>
+          <h3><?php echo $Estado; ?> <?php echo $Ciudad; ?></h3>
+          <h3><?php echo $Direccion; ?> <?php echo $CP; ?></h3>
+          <p><?php if($VentaRenta == 1){echo "Venta";}else{echo "Renta";} ?></p>
+          <p><?php if($TipoInmueble == 1){echo "Casa";}else{echo "Departamento";} ?></p>
+          <p>Metros cuadrados: <?php echo $MetrosCuadrados; ?>m²</p>
+          <p>N&uacute;mero de dormitorios: <?php echo $NumeroDormitorio; ?></p>
+          <p>N&uacute;mero de baños: <?php echo $NumeroBanios; ?></p>
+          <p>Longitud: <?php echo $Longitud; ?> Latitud: <?php echo $Latitud; ?></p>
           <p><b>Descripci&oacute;n de inmueble</b></p>
-          <p id="Descripcion">Esta bonito </p>
+          <p><?php echo $Descripcion; ?></p>
         </div>
         <div class="col-md-5 col-lg-4 order-md-last informacionExtra">
           <center>
-            <p><span id="ImagenUsuario"></span></p>
-            <p><h2><span id="Usuario"></span></h2></p>
-            <p><h3>Calificaci&oacute;n: <span id="Calificacion"></span></h3></p>
-            <p><h4><span id="Correo"></span></h4></p>
-            <p><h4><span id="Telefono"></span></h4></p>
-            <span id="Mensaje"><button type="button" class="w-100 btn btn-lg btn-light">Mandar mensaje al vendedor</button></span>
+            <p><img width="250" height="250" <?php echo "src=\"data:image/jpg;base64, ".base64_encode($ImagenUsuario)."\""; ?>></p>
+            <p><h2><?php echo $Usuario; ?></h2></p>
+            <p><?php echo $Calificacion; ?></p>
+            <p><h4><?php echo $Correo; ?></h4></p>
+            <p><h4><?php echo $Telefono; ?></h4></p>
+            <?php echo "<a href=\"./enviarMensaje.php?receptor=".$Usuario."&IdInmueble=".$idInmueble."\">"; ?><button type="button" class="w-100 btn btn-lg btn-light">Mandar mensaje al vendedor</button></a>
             <p></p>
             <button type="button" class="w-100 btn btn-lg btn-light">Guardar a favoritos</button>
             <p></p>
@@ -86,138 +177,7 @@
           </center>
         </div>
       </div>
-
     </header>
-
-    <?php
-      $idInmueble = $_GET['IdInmueble'];
-
-      $servername = "localhost";
-      $username = "root";
-      $password = "";
-
-      $conn = mysqli_connect($servername, $username, $password);
-
-      if(!$conn){
-        die("Conexion fallida: ".mysqli_connect_error());
-      }
-
-      mysqli_select_db($conn, "MASK");
-
-      $sql = "SELECT * FROM Inmueble WHERE IdInmueble=".$idInmueble."";
-
-      $result = mysqli_query($conn, $sql);
-
-      if(mysqli_num_rows($result) > 0){
-        $row = mysqli_fetch_assoc($result);
-        $NumeroDormitorio = $row['NumeroDormitorios'];
-        $NumeroBanios = $row['NumeroBanios'];
-        $VentaRenta = $row['VentaRenta'];
-        $TipoInmueble = $row['TipoInmueble'];
-        $Titulo = $row['Titulo'];
-        $Estado = $row['Estado'];
-        $Ciudad = $row['Ciudad'];
-        $Direccion = $row['Direccion'];
-        $CP = $row['CP'];
-        $MetrosCuadrados = $row['MetrosCuadrados'];
-        $Descripcion = $row['Descripcion'];
-        $Costo = $row['Costo'];
-        $Moneda = $row['Moneda'];
-        $Longitud = $row['Longitud'];
-        $Latitud = $row['Latitud'];
-        $IdUsuario = $row['IdUsuario'];
-        $Visitas = $row['Visitas'];
-        $Visitas = $Visitas + 1;
-        echo "<script>
-                document.getElementById(\"NumeroDormitorio\").innerHTML=\"".$NumeroDormitorio."\";
-                document.getElementById(\"NumeroBanios\").innerHTML=\"".$NumeroBanios."\";
-                if(\"".$VentaRenta."\"==\"1\"){
-                  document.getElementById(\"VentaRenta\").innerHTML=\"Venta\";
-                }
-                else{
-                  document.getElementById(\"VentaRenta\").innerHTML=\"Renta\";
-                }
-                document.getElementById(\"TipoInmueble\").innerHTML=\"".$TipoInmueble."\";
-                if(\"".$TipoInmueble."\"==\"1\"){
-                  document.getElementById(\"TipoInmueble\").innerHTML=\"Casa\";
-                }
-                else{
-                  document.getElementById(\"TipoInmueble\").innerHTML=\"Departamento\";
-                }
-                document.getElementById(\"Titulo\").innerHTML=\"".$Titulo."\";
-                document.getElementById(\"Estado\").innerHTML=\"".$Estado."\";
-                document.getElementById(\"Ciudad\").innerHTML=\"".$Ciudad."\";
-                document.getElementById(\"Direccion\").innerHTML=\"".$Direccion."\";
-                document.getElementById(\"CP\").innerHTML=\"".$CP."\";
-                document.getElementById(\"MetrosCuadrados\").innerHTML=\"".$MetrosCuadrados."\";
-                document.getElementById(\"Descripcion\").innerHTML=\"".$Descripcion."\";
-                document.getElementById(\"Costo\").innerHTML=\"".$Costo."\";
-                if(\"".$Moneda."\"==\"1\"){
-                  document.getElementById(\"Moneda\").innerHTML=\"Pesos mexicanos\";
-                }
-                else if(\"".$Moneda."\"==\"2\"){
-                  document.getElementById(\"Moneda\").innerHTML=\"Dolar\";
-                }
-                else if(\"".$Moneda."\"==\"3\"){
-                  document.getElementById(\"Moneda\").innerHTML=\"Euro\";
-                }
-                else if(\"".$Moneda."\"==\"4\"){
-                  document.getElementById(\"Moneda\").innerHTML=\"Yen\";
-                }
-                document.getElementById(\"Longitud\").innerHTML=\"".$Longitud."\";
-                document.getElementById(\"Latitud\").innerHTML=\"".$Latitud."\";
-              </script>";
-      }
-      $sql = "UPDATE inmueble SET Visitas = ".$Visitas." WHERE idInmueble = ".$idInmueble."";
-      if(mysqli_query($conn, $sql)){
-      }
-      $sql = "SELECT * FROM Usuario WHERE IdUsuario = ".$IdUsuario."";
-      $result = mysqli_query($conn, $sql);
-      if(mysqli_num_rows($result) > 0){
-        $row = mysqli_fetch_assoc($result);
-        $Usuario = $row['Usuario'];
-        $Correo = $row['Correo'];
-        $Calificacion = $row['Calificacion'];
-        $Telefono = $row['Telefono'];
-        $ImagenUsuario = $row['FotoUsuario'];
-        echo "<script>
-                document.getElementById(\"ImagenUsuario\").innerHTML=\"<img width=\\\"250\\\" height=\\\"250\\\" src=\\\"data:image/jpg;base64, ".base64_encode($ImagenUsuario)."\\\">\";
-                document.getElementById(\"Usuario\").innerHTML=\"".$Usuario."\";
-                document.getElementById(\"Calificacion\").innerHTML=\"".$Calificacion."\";
-                document.getElementById(\"Correo\").innerHTML=\"".$Correo."\";
-                document.getElementById(\"Telefono\").innerHTML=\"".$Telefono."\";
-              </script>";
-      }
-      $sql = "SELECT * FROM InmuebleFoto WHERE IdInmueble=".$idInmueble."";
-      $result = mysqli_query($conn, $sql);
-      if(mysqli_num_rows($result) > 0){
-        $Texto = "<script>
-          document.getElementById(\"orden\").innerHTML=\"<button type=\\\"button\\\" data-bs-target=\\\"#myCarousel\\\" data-bs-slide-to=\\\"0\\\" class=\\\"active\\\" aria-current=\\\"true\\\" aria-label=\\\"Slide 1\\\"></button>";
-        $Contador = 1;
-        while ($Contador < mysqli_num_rows($result)) {
-          $Texto = $Texto."<button type=\\\"button\\\" data-bs-target=\\\"#myCarousel\\\" data-bs-slide-to=\\\"".$Contador."\\\" aria-label=\\\"Slide ".($Contador+1)."\\\"></button>";
-          $Contador = $Contador + 1;
-        }
-        $Texto = $Texto."\";
-                </script>";
-        echo $Texto;
-        $Activado = "active";
-        $Texto = "<script>
-          document.getElementById(\"imagenes\").innerHTML=\"";
-        while($row = mysqli_fetch_array($result)){
-            $Texto = $Texto."<div class=\\\"carousel-item ".$Activado."\\\"><img class=\\\"d-block w-100\\\" src=\\\"data:image/jpg;base64, ".base64_encode($row['Foto'])."\\\"></div>";
-            $Activado = "";
-        }
-        $Texto = $Texto."\";
-                </script>";
-        echo $Texto;
-        echo "<script>
-                document.getElementById(\"Mensaje\").innerHTML=\"<a href=\\\"./enviarMensaje.php?receptor=".$Usuario."&IdInmueble=".$idInmueble."\\\"><button type=\\\"button\\\" class=\\\"w-100 btn btn-lg btn-light\\\">Mandar mensaje al vendedor</button></a>\"
-              </script>";
-      }
-    ?>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
   </body>
 </html>
