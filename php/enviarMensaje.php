123f9ee3ac7922b9ff24d2ae5c4c1dbae2fbb612
Sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2021-06-08 16:06:24 -0500

    Se agrego la censura con 100 insultos

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -7,23 +7,195 @@ $usuario = $_SESSION['usuario'];
 $mensaje = $_POST['mensaje'];
 $receptor = $_GET['receptor'];
 $idInmueble = $_GET['idInmueble'];
-if(strlen($mensaje)>14){
+
     $mensajeAjuste = $mensaje;
+    $palabra = "";
     $mensajeAjuste = "";
     $contador = 0;
     for($i=0;$i<strlen($mensaje);$i++){
         if($mensaje[$i]!=" "){
             $contador++;
+            $palabra = $palabra.$mensaje[$i];            
+        
         }else{
-            $contador = 0;
+            $contador = 0;                    
+            $palabra = buscarInsulto($palabra);
+            $mensajeAjuste = $mensajeAjuste." ".$palabra;
+            $palabra = "";
         }
         if($contador==14){
             $mensajeAjuste = $mensajeAjuste."\n";
             $contador = 0;
         }
-        $mensajeAjuste = $mensajeAjuste.$mensaje[$i];
     }
+    $palabra = buscarInsulto($palabra);
+    $mensajeAjuste = $mensajeAjuste." ".$palabra;
     $mensaje = $mensajeAjuste;
+
+
+function buscarInsulto($palabra){
+    $palabra = strtolower($palabra);
+    $palabraSola = "";
+    $signo = "";
+    for($i=0;$i<strlen($palabra);$i++){
+        $c = $palabra[$i];
+        if($c=="?" || $c=="," || $c=="." || $c=="!"){
+            $signo = $signo.$c;
+        }else{
+            $palabraSola = $palabraSola.$c;            
+        }
+    }
+    switch ($palabraSola) {
+        case "puto";
+        case "nigga";
+        case "verga";
+        case "puta";
+        case "pendejo";
+        case "pendeja";
+        case "zorra";
+        case "prostituta";
+        case "chinga";
+        case "coño";
+        case "jodete";
+        case "v3rga";
+        case "berga";
+        case "verg4";
+        case "put4";
+        case "put0";
+        case "piruja";
+        case "mierda";
+        case "fuck";
+        case "shit";
+        case "pendej4";
+        case "p3ndeja";
+        case "pend3ja";
+        case "p3nd3j4";
+        case "pendej0";
+        case "pend3jo";
+        case "gay";
+        case "maricon";
+        case "maric0n";
+        case "m4ricon";
+        case "m4ric0n";
+        case "jot0";
+        case "j0to";
+        case "j0t0";
+        case "vergota";
+        case "putona";
+        case "putita";
+        case "putito";
+        case "putote";
+        case "putota";
+        case "chupamela";
+        case "ano";
+        case "anal";
+        case "bukake";
+        case "jodete";
+        case "tonto";
+        case "tonta";
+        case "t0nt0";
+        case "t0nto";
+        case "tont0";
+        case "huérfano";
+        case "huérfana";
+        case "huerfano";
+        case "pinche";        
+        case "estupido";
+        case "estupida";
+        case "mejicano";
+        case "mejicana";
+        case "frijolero";
+        case "frijolera";
+        case "tortillera";
+        case "tortillero";
+        case "idiota";
+        case "imbécil";
+        case "invecil";
+        case "invesil";
+        case "mongolo";
+        case "mongolito";
+        case "inbecil";
+        case "inbesil";
+        case "imbecil";
+        case "imbesil";
+        case "imvesil";
+        case "imvecil";
+        case "huerfana";
+        case "simp";
+        case "incel";
+        case "insel";
+        case "fuckboy";
+        case "poronga";
+        case "amlo";
+        case "prieto";
+        case "prieta";
+        case "maricones";
+        case "puñetas";
+        case "putos";
+        case "putas";
+        case "chingas";
+        case "chingo";
+        case "pollon";
+        case "vaginon";
+        case "semen";
+        case "cemen";
+        case "follar";
+        case "follaron";
+        case "cojieron";
+        case "cojeré";
+        case "cojere";
+        case "culear";
+        case "culearé";
+        case "culeare";
+        case "chingamos";
+        case "cojemos";
+        case "culeamos";
+        case "culeas";
+        case "putita";
+        case "prostitua?";
+        case "golfa";
+        case "cojer";
+        case "pene";
+        case "nepe";
+        case "vagina";
+        case "vergazo";
+        case "vergaso";
+        case "bergaso";
+        case "bergazo";
+        case "putazo";
+        case "putaso";
+        case "putaz0";
+        case "culera";
+        case "culero";
+        case "culo";
+        case "cul0";
+        case "sorra";
+        case "z0rra";
+        case "s0rra";
+        case "zorr4";
+        case "z0rr4";
+            $palabra = censurar($palabraSola).$signo;
+                break;
+        
+        default;
+            $palabra = $palabra.$signo;
+            break;
+    }
+    return $palabra;
+}
+
+function censurar($palabra){
+    $newPalabra = "";
+    for($i=0;$i<strlen($palabra);$i++){
+        if($i==0){
+            $newPalabra = $palabra[$i];
+        }elseif($i==(strlen($palabra)-1)){
+            $newPalabra = $newPalabra.$palabra[$i];
+        }else{
+            $newPalabra = $newPalabra."*";
+        }
+    }
+    return $newPalabra;
 }
 
 if(strlen($mensaje)>12){
